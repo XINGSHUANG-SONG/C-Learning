8193ad862aa51b432924aa3640656581946e7e93
liao-meikan <3520574791@qq.com>
2026-07-30 13:29:22 +0800

    refactor:remove duplicated code in diamond program

--- diamond/diamond.c
+++ diamond/diamond.c
@@ -11,27 +11,18 @@ int print_diamond(int n, char m){
         if(i <= (n + 1) / 2){
             left = (n + 1) / 2 - i + 1;
             right = n - (n + 1) / 2 + i;
-            for(int j = left; j <= right; j++){
-                if(left > 1 && j == left){
-                    for(int count = 1;count < left;count++){
-                        printf("       ");
-                    }
-                };
-                printf("%c      ", m);
-                if(j == right)printf("\n");
-            }
-        }else if(i > (n + 1) / 2){
+        }else{
             left = i - (n + 1) / 2 + 1;
             right = n - i + (n + 1) / 2;
-            for(int j = left; j <= right; j++){
-                if(left > 1 && j == left){
-                    for(int count = 1;count < left;count++){
-                        printf("       ");
-                    }
+        }
+        for(int j = left; j <= right; j++){
+            if(left > 1 && j == left){
+                for(int count = 1;count < left;count++){
+                    printf("       ");
                 }
-                printf("%c      ", m);
-                if(j == right)printf("\n");
             }
+            printf("%c      ", m);
+            if(j == right)printf("\n");
         }
     }
     return 0;
