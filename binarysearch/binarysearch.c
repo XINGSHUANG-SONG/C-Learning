9c7024d7e522c7b5b126064abaed25fe33b3ec30
liao-meikan <3520574791@qq.com>
2026-08-13 21:30:57 +0800

    feat:Implement binarysearch program

---
+++ binarysearch/binarysearch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define LEN 8
+int a[LEN] = { 1, 2, 2, 2, 5, 6, 8, 9 };
+
+int binarysearch(int number)
+{
+	int mid, start = 0, end = LEN - 1;
+
+	while (start <= end) {
+		mid = (start + end) / 2;
+		if (a[mid] < number) {
+			start = mid + 1;
+        } else if (a[mid] > number) {
+			end = mid - 1;
+        } else {
+            if (a[mid - 1] == a[mid] && mid > 0) {
+                for (int i = mid - 1; i >= 0; i--) {
+                    if (a[i] != a[mid])
+                        return i + 1;
+                }
+            }
+			return mid;
+        }    
+	}
+	return -1;
+}
+
+int main(void)
+{
+	printf("%d\n", binarysearch(2));
+	return 0;
+}
