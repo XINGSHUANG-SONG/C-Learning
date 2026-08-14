adc1ba6cf9ed2fce94cf8b6aca57046d65eaa270
liao-meikan <3520574791@qq.com>
2026-08-14 18:47:57 +0800

    refactor:optimize mysqrt program

--- binarysearch/mysqrt.c
+++ binarysearch/mysqrt.c
@@ -4,9 +4,9 @@ double mysqrt(double y) {
     double x, start = 0.0, end = ((y >= 1) ? y : 1);
     while (1) {
         x = (start + end) / 2.0;
-        if ((x*x - y) >= 0.001)
+        if ((x*x - y) >= 0.0000000001)
             end = x;
-        else if((x*x - y) <= -0.001)
+        else if((x*x - y) <= -0.0000000001)
             start = x;
         else
             return x;
@@ -15,12 +15,15 @@ double mysqrt(double y) {
 }
 
 int main(void) {
-    double y;
-    printf("Please input a number to y.(y > 0)\n");
-    if (scanf("%lf", &y) != 1 || y < 0) {
-        printf("Invalid input.\n");
-        return 1;
+    while (1) {
+        double y;
+        printf("Recommended range: 1e-6 to 1e8 for obtaining more accurate results.\n");        
+        printf("Please input a number to y.(y > 0)\n");
+        if (scanf("%lf", &y) != 1 || y <= 0) {
+            printf("Invalid input.\n");
+            return 1;
+        }
+        printf("%0.10lf\n", mysqrt(y));
     }
-    printf("%f\n", mysqrt(y));
     return 0;
 }
